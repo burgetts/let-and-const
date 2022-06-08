47eddfdf86f9cacd3e9c7afe8b659a2879d6dc46
Stevie Burgett <stevieburgett@Stevies-MacBook-Pro.local>
2022-06-08 10:28:23 -0600

    first commit

---
+++ let-and-const-practice.js
@@ -0,0 +1,6 @@
+// var PI = 3.14
+// PI = 42
+
+const pi = 3.14;
+
+// var is subject to variable hoisting, so it can be used before it is declared in the code. It also has function scope and can be redeclared at ant time, while neither let nor const can be redeclared. Let and var can be reassigned, but const cannot.
